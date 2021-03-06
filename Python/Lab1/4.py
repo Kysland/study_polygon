2c09e8f261df6052ddcbdbf7102fc4235910723c
Fyodor Garus <kyslands@gmail.com>
2021-03-07 01:48:06 +0300

    Python

--- Python/Lab1/4.py
+++ Python/Lab1/4.py
@@ -12,15 +12,17 @@ c = int(input("Enter number #c: "))
 D = b ** 2 - 4 * a * c
 print("discriminant = ", D)
 
-if D > 0:
-    #x1 = -b + D**(1./2) / 2 * a
-    #x2 = -b + D**(1./2) / 2 * a
-    x1 = (-b + sqrt(D)) / 2 * a
-    x2 = (-b + sqrt(D)) / 2 * a
+if a == 0:
+    print("eto ne ^2 yravnenie")
+elif D > 0:
+    #x1 = -b + D**(1./2) / (2 * a)
+    #x2 = -b + D**(1./2) / (2 * a)
+    x1 = (-b + sqrt(D)) / (2 * a)
+    x2 = (-b - sqrt(D)) / (2 * a)
     print("x1 = ", x1)
     print("x2 = ", x2)
 elif D == 0:
-    x = -b / 2 * a
+    x = -b / (2 * a)
     print("x = ", x)
 else:
     print("no roots")
